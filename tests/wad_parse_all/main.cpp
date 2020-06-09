4b9288d94c9492a616b95d0323a5c26c10ebba3a
Abit Gray <AbitTheGray@gmail.com>
2020-06-09 13:46:16 +0200

    WadParse_All test prints count of items

--- tests/wad_parse_all/main.cpp
+++ tests/wad_parse_all/main.cpp
@@ -18,19 +18,30 @@ int main()
 
             auto wadIt = WadParser(dir.path());
 
+            std::size_t countTexture = 0;
+            std::size_t countImage = 0;
+            std::size_t countFont = 0;
+
             for(const auto& item : wadIt.GetItems())
             {
                 switch(item.Type)
                 {
                     case WadParser::ItemType::MipMapTexture:
+                        countTexture++;
+                        continue;
                     case WadParser::ItemType::Image:
+                        countImage++;
+                        continue;
                     case WadParser::ItemType::Font:
+                        countFont++;
                         continue;
                     default:
                         break;
                 }
                 std::cout << item.Name << " = " << static_cast<uint32_t>(static_cast<uint8_t>(item.Type)) << std::endl;
             }
+
+            std::cout << "Texture: " << countTexture << ", Image: " << countImage << ", Font: " << countFont << std::endl;
         }
         catch(std::exception& ex)
         {
