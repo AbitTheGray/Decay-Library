47152e589cd48b8d4f9065cf1bc414a69cbb268d
Abit Gray <AbitTheGray@gmail.com>
2020-06-10 17:27:01 +0200

    Added weight to MIME Weight (80) matches application/x-doom Added script to uninstall mime

---
+++ linux/uninstall-mime.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Remove old
+xdg-mime uninstall 'mime/goldsrc-bsp30.xml'
+xdg-mime uninstall 'mime/goldsrc-wad2.xml'
+xdg-mime uninstall 'mime/goldsrc-wad3.xml'
+
+echo "Don't forget to run it as booth your user and root"
