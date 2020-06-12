946b811866c262e6b350e01290a6981288ad86c2
Abit Gray <AbitTheGray@gmail.com>
2020-06-12 13:57:38 +0200

    First attempt to export BspTree as Flattened OBJ

--- src/Decay/Bsp/BspTree.hpp
+++ src/Decay/Bsp/BspTree.hpp
@@ -60,5 +60,43 @@ namespace Decay::Bsp
         void UpdateNode_ChildIndex(const std::shared_ptr<SmartNode>& smartNode, int16_t childIndex);
 
         void Insert_Visual(const std::shared_ptr<SmartNode>& smartNode, const BspFile::Node& node);
+
+    public:
+        [[nodiscard]] inline std::map<uint16_t, std::vector<uint16_t>> FlattenIndices() const
+        {
+            std::map<uint16_t, std::vector<uint16_t>> indices = {};
+            {
+                FlattenIndices(MainNode, indices);
+            }
+            return indices;
+        }
+    private:
+        static void FlattenIndices(const std::shared_ptr<SmartNode>& smartNode, std::map<uint16_t, std::vector<uint16_t>>& indices)
+        {
+            const std::map<uint16_t, std::vector<uint16_t>>& smartIndices = smartNode->Indices;
+            for(const auto& it : smartIndices)
+            {
+                std::vector<uint16_t>& ind = indices[it.first];
+                ind.insert(ind.end(), it.second.begin(), it.second.end());
+            }
+
+            for(const std::shared_ptr<SmartLeaf>& leaf : smartNode->Leaves)
+                FlattenIndices(leaf, indices);
+
+            for(const std::shared_ptr<SmartNode>& subNode : smartNode->ChildNodes)
+                FlattenIndices(subNode, indices);
+        }
+        inline static void FlattenIndices(const std::shared_ptr<SmartLeaf>& smartLeaf, std::map<uint16_t, std::vector<uint16_t>>& indices)
+        {
+            const auto& smartIndices = smartLeaf->Indices;
+            for(const auto& it : smartIndices)
+            {
+                std::vector<uint16_t>& ind = indices[it.first];
+                ind.insert(ind.end(), it.second.begin(), it.second.end());
+            }
+        }
+
+    public:
+        void ExportFlatObj(const std::filesystem::path& filename);
     };
 }
---
+++ tests/bsp_export_obj/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+#include <Decay/Bsp/BspFile.hpp>
+#include <Decay/Bsp/BspTree.hpp>
+
+int main()
+{
+    using namespace Decay::Bsp;
+
+    auto bsp = std::make_shared<BspFile>("../../../half-life/cstrike/maps/de_dust2.bsp");
+
+    auto tree = std::make_shared<BspTree>(bsp);
+    tree->ExportFlatObj("de_dust2.obj");
+}
