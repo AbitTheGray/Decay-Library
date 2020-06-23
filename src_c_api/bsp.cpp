dfdd48df38133a33612c9590595ad31345454ec7
Abit Gray <AbitTheGray@gmail.com>
2020-06-23 14:06:14 +0200

    Added `nullptr` returns to some functions

--- src_c_api/bsp.cpp
+++ src_c_api/bsp.cpp
@@ -17,8 +17,24 @@ inline static std::shared_ptr<BspFile> GetBspPointer(bsp_file* bspFile)
 
 bsp_file* bsp_file_load(const char* path)
 {
-    std::shared_ptr<BspFile>& ptr = BspFiles.emplace_back(std::make_shared<BspFile>(path));
-    return ptr.get();
+    if(path == nullptr)
+        return nullptr;
+
+    std::filesystem::path filename(path);
+    if(!std::filesystem::exists(filename))
+        return nullptr; // File not found
+    if(!std::filesystem::is_regular_file(filename))
+        return nullptr; // Path target is not a file
+
+    try
+    {
+        return BspFiles.emplace_back(std::make_shared<BspFile>(path)).get();
+    }
+    catch(std::exception& ex)
+    {
+        std::cerr << "bsp_file_load(\"" << path << "\") resulted in: " << ex.what() << std::endl;
+        return nullptr;
+    }
 }
 
 void bsp_file_free(bsp_file* bspFile)
@@ -44,8 +60,15 @@ bsp_tree* bsp_tree_create(bsp_file* bspFile)
     if(bsp == nullptr)
         return nullptr;
 
-    std::shared_ptr<BspTree>& ptr = BspTrees.emplace_back(std::make_shared<BspTree>(bsp));
-    return ptr.get();
+    try
+    {
+        return BspTrees.emplace_back(std::make_shared<BspTree>(bsp)).get();
+    }
+    catch(std::exception& ex)
+    {
+        std::cerr << "bsp_file_load(\"" << path << "\") resulted in: " << ex.what() << std::endl;
+        return nullptr;
+    }
 }
 
 void bsp_tree_free(bsp_tree* bspTree)
--- src_c_api/bsp.h
+++ src_c_api/bsp.h
@@ -8,15 +8,35 @@ extern "C"
     typedef void bsp_tree;
 
     /// Load BSP from filesystem
+    /// Returns `nullptr` if file was not found or there was problem with loading
     bsp_file* bsp_file_load(const char* path);
     /// Free BSP pointer from `bsp_file_load`
     void bsp_file_free(bsp_file* bspFile);
 
     /// Constructs BSP Tree from BSP map
-    /// Returns `nullptr` when incorrect `bspFile` is supplied.
+    /// Returns `nullptr` when incorrect `bspFile` is supplied or there was problem with parsing it.
     bsp_tree* bsp_tree_create(bsp_file* bspFile);
     /// Free BSP Tree pointer from `bsp_tree_create`
     void bsp_tree_free(bsp_tree* bspTree);
+
+    void bsp_free(bsp_file* bspFile, bsp_tree* bspTree)
+    {
+        bsp_file_free(bspFile);
+        bsp_tree_free(bspTree);
+    }
+    bsp_tree* bsp_tree_load(const char* path)
+    {
+        bsp_file* bspFile = bsp_file_load(path);
+        if(bspFile == nullptr)
+            return nullptr;
+
+        bsp_tree* bspTree = bsp_tree_create(bspFile);
+
+        bsp_file_free(bspFile);
+
+        return bspTree; // May be ` nullptr`
+    }
+
 
     //TODO extract textures from BSP
     //TODO get geometry from BSP Tree
