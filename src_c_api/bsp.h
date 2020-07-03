d403f3bc60adb4d4a8da2e8e5b610d4220a7ee21
Abit Gray <AbitTheGray@gmail.com>
2020-07-03 17:45:57 +0200

    Texture indices from Bsp Model

--- src_c_api/bsp.h
+++ src_c_api/bsp.h
@@ -7,6 +7,7 @@ typedef Decay::Bsp::BspFile bsp_file;
 typedef Decay::Bsp::BspTree bsp_tree;
 
 typedef Decay::Bsp::BspTree::Vertex bsp_vertex;
+typedef Decay::Bsp::BspTree::Model bsp_model;
 #else
 typedef struct {char dummy;} bsp_file;
 typedef struct {char dummy;} bsp_tree;
@@ -17,6 +18,7 @@ typedef struct
     float u, v;
     float s, t;
 } bsp_vertex;
+typedef struct {char dummy;} bsp_model;
 #endif
 
 #ifdef __cplusplus
@@ -26,21 +28,28 @@ extern "C"
 
     /// Load BSP from filesystem
     /// Returns `NULL` if file was not found or there was problem with loading
+    /// Returned pointer must be freed using `bsp_file_free(bsp_tree*)` (not required if `NULL`).
     bsp_file* bsp_file_load(const char* path);
     /// Free BSP pointer from `bsp_file_load`
     void bsp_file_free(bsp_file* bspFile);
 
     /// Constructs BSP Tree from BSP map
     /// Returns `NULL` when incorrect `bspFile` is supplied or there was problem with parsing it.
+    /// Returned pointer must be freed using `bsp_tree_free(bsp_tree*)` (not required if `NULL`).
     bsp_tree* bsp_tree_create(bsp_file* bspFile);
     /// Free BSP Tree pointer from `bsp_tree_create`
     void bsp_tree_free(bsp_tree* bspTree);
 
+    /// Utility function to free `bsp_file*` and `bsp_tree*` using same function.
     void bsp_free(bsp_file* bspFile, bsp_tree* bspTree)
     {
         bsp_file_free(bspFile);
         bsp_tree_free(bspTree);
     }
+
+    /// Loads `bsp_tree*` directly from file.
+    /// May return `NULL`.
+    /// Returned pointer must be freed using `bsp_tree_free(bsp_tree*)` (not required if `NULL`).
     bsp_tree* bsp_tree_load(const char* path)
     {
         bsp_file* bspFile = bsp_file_load(path);
@@ -64,9 +73,31 @@ extern "C"
     /// Pointer lifetime is same as `bsp_tree*`
     bsp_vertex* bsp_vertices(bsp_tree* bspTree);
 
+    /// Get all models from BSP Tree.
+    /// Returns number of models.
+    /// Caller has to allocate array of `bsp_model*` and pass it as `models` argument. Can be `NULL` to not write values.
+    /// No freeing is required.
+    /// `bsp_model*` lifetime is same as used `bsp_tree*`.
+    int bsp_get_models(bsp_tree* bspTree, bsp_model** models);
+
+    /// Get model by index from BSP Tree.
+    /// `bsp_model*` lifetime is same as used `bsp_tree*`.
+    bsp_model* bsp_get_model(bsp_tree* bspTree, int modelId);
+    /// Get main model (static world) from BSP Tree .
+    /// `bsp_model*` lifetime is same as used `bsp_tree*`.
+    bsp_model* bsp_get_world_model(bsp_tree* bspTree)
+    {
+        return bsp_get_model(bspTree, 0);
+    }
+
+    int bsp_model_get_indices(bsp_model* model, int textureIndex, int* indices);
+
+    /// Get textures used by specified `bsp_model`.
+    /// Returns number of textures.
+    /// If `textures` is not `NULL` then texture indexes are written into it. Caller has to allocate enough memory.
+    int bsp_model_textures(bsp_model* model, int* textures);
 
     //TODO extract textures from BSP
-    //TODO get geometry from BSP Tree
 
 #ifdef __cplusplus
 };
